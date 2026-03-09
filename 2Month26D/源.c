eb336953aef2e04822aa2714c58dbb7329e6ae70
GuoMingzhi <guomingzhi2020@163.com>
2026-03-09 10:50:43 +0800

    add test.txt

---
+++ 2Month26D/源.c
@@ -0,0 +1,14 @@
+//#include <iostream>
+//#include <string>
+#include<stdio.h>
+#include <windows.h>
+int main()
+{
+	Sleep(5000);
+	int a = 10;
+	int b = 0;
+	int c = a / b;
+
+	printf("hello world!");
+	return 0;
+}
